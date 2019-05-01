b7202169d6fc7cd76dcd86ef92301458824eb76c
Kensuke Niitsuma <niitsken.git@gmail.com>
2019-05-01 23:52:42 +0900

    set config and add libraries

---
+++ app/src/main/java/com/example/twitterclone/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kensuke.twitterclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
