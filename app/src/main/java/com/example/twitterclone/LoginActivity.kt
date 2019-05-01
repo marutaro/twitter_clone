bb139cc663b3aee4f404eef26a05be9029d88edf
Kensuke Niitsuma <niitsken.git@gmail.com>
2019-05-02 01:42:45 +0900

    login design

--- app/src/main/java/com/example/twitterclone/LoginActivity.kt
+++ app/src/main/java/com/example/twitterclone/LoginActivity.kt
@@ -2,11 +2,20 @@ package com.example.kensuke.twitterclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onLogin(v: View) {
+
+    }
+
+    fun goToSignup(v: View) {
+
     }
 }
