86a4cc94c2a4c65b50062f40428a3eed5ea541de
Kensuke Niitsuma <niitsken.git@gmail.com>
2019-05-09 01:58:17 +0900

    uploading functionality for profile image

--- app/src/main/java/com/example/twitterclone/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/twitterclone/activities/ProfileActivity.kt
@@ -1,19 +1,19 @@
 package com.example.twitterclone.activities
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import com.example.kensuke.twitterclone.R
-import com.example.twitterclone.util.DATA_USERS
-import com.example.twitterclone.util.DATA_USER_EMAIL
-import com.example.twitterclone.util.DATA_USER_USERNAME
-import com.example.twitterclone.util.User
+import com.example.twitterclone.util.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_profile.*
 import java.lang.Exception
 
@@ -22,7 +22,9 @@ class ProfileActivity : AppCompatActivity() {
 
     private val firebaseAuth = FirebaseAuth.getInstance()
     private val firebaseDB = FirebaseFirestore.getInstance()
+    private val firebaseStorage = FirebaseStorage.getInstance().reference
     private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    private var imageUrl: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +36,12 @@ class ProfileActivity : AppCompatActivity() {
 
         profileProgressLayout.setOnTouchListener { view, motionEvent -> true }
 
+        profilePhoto.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_CODE_PHOTO)
+        }
+
         populateInfo()
     }
 
@@ -44,6 +52,12 @@ class ProfileActivity : AppCompatActivity() {
                 val user = documentSnapshot.toObject(User::class.java)
                 usernameEditText.setText(user?.username, TextView.BufferType.EDITABLE)
                 emailEditText.setText(user?.email, TextView.BufferType.EDITABLE)
+
+                imageUrl = user?.imageUrl
+                imageUrl?.let {
+                    profilePhoto.loadUrl(user?.imageUrl, R.drawable.logo)
+                }
+
                 profileProgressLayout.visibility = View.GONE
             }
             .addOnFailureListener { e: Exception ->
@@ -72,6 +86,45 @@ class ProfileActivity : AppCompatActivity() {
                 Toast.makeText(this, "Update failed. Please try again.", Toast.LENGTH_SHORT).show()
                 profileProgressLayout.visibility = View.GONE
             }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_PHOTO) {
+            storeImage(data?.data)
+        }
+    }
+
+    fun storeImage(imageUri: Uri?) {
+        imageUri?.let {
+            Toast.makeText(this, "Uploading...", Toast.LENGTH_SHORT).show()
+            profileProgressLayout.visibility = View.VISIBLE
+            val filePath = firebaseStorage.child(DATA_IMAGES).child(userId!!)
+            filePath.putFile(imageUri)
+                .addOnSuccessListener {
+                    filePath.downloadUrl
+                        .addOnSuccessListener { uri ->
+                            val url = uri.toString()
+                            firebaseDB.collection(DATA_USERS).document(userId!!).update(DATA_USER_IMAGE_URL, url)
+                                .addOnSuccessListener {
+                                    imageUrl = url
+                                    profilePhoto.loadUrl(imageUrl, R.drawable.logo)
+                                }
+                            profileProgressLayout.visibility = View.GONE
+                        }
+                        .addOnFailureListener {
+                            onUploadFailure()
+                        }
+                }
+                .addOnFailureListener {
+                    onUploadFailure()
+                }
+        }
+    }
+
+    fun onUploadFailure() {
+        Toast.makeText(this, "Image upload failed. Please try again later.", Toast.LENGTH_SHORT).show()
+        profileProgressLayout.visibility = View.GONE
     }
 
     fun onSignout(v: View) {
---
+++ app/src/main/java/com/example/twitterclone/util/Util.kt
@@ -0,0 +1,29 @@
+package com.example.twitterclone.util
+
+import android.content.Context
+import android.support.v4.widget.CircularProgressDrawable
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.kensuke.twitterclone.R
+
+fun ImageView.loadUrl(url: String?, errorDrawable: Int = R.drawable.empty) {
+    context?.let {
+        val options = RequestOptions()
+            .placeholder(progressDrawable(context))
+            .error(errorDrawable)
+
+            Glide.with(context.applicationContext)
+                .load(url)
+                .apply(options)
+                .into(this)
+    }
+}
+
+fun progressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 5f
+        centerRadius = 30f
+        start()
+    }
+}
